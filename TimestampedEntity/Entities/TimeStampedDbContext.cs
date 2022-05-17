262589670a6d2d1df31f3c0033d254e2c633e70e
Corentin Jouffe <corentin.jouffe@gmail.com>
2022-05-17 16:43:46 +0200

    Modification TimeStampedDbContext.cs

--- TimestampedEntity/Entities/TimeStampedDbContext.cs
+++ TimestampedEntity/Entities/TimeStampedDbContext.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -9,119 +10,37 @@ namespace TimestampedEntity.Entities;
 
 public class TimeStampedDbContext : DbContext
 {
-    private static void SetCreatedOnDateTime(object entity)
+    protected TimeStampedDbContext(DbContextOptions options) : base(options)
     {
-        if (entity is TimestampedEntity timestampedEntity)
-        {
-            var now = DateTime.Now;
-            timestampedEntity.CreatedOn = now;
-            timestampedEntity.UpdatedOn = now;
-        }
-    }
-    
-    private static void SetUpdatedOnDateTime(object entity)
-    {
-        if (entity is TimestampedEntity timestampedEntity)
-        {
-            timestampedEntity.UpdatedOn = DateTime.Now;
-        }
-    }
-
-    public override EntityEntry Add(object entity)
-    {
-        SetCreatedOnDateTime(entity);
-        return base.Add(entity);
-    }
-
-    public override EntityEntry<TEntity> Add<TEntity>(TEntity entity)
-    {
-        SetCreatedOnDateTime(entity);
-        return base.Add(entity);
-    }
-
-    public override ValueTask<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = new CancellationToken())
-    {
-        SetCreatedOnDateTime(entity);
-        return base.AddAsync(entity, cancellationToken);
-    }
-
-    public override ValueTask<EntityEntry> AddAsync(object entity, CancellationToken cancellationToken = new CancellationToken())
-    {
-        SetCreatedOnDateTime(entity);
-        return base.AddAsync(entity, cancellationToken);
-    }
-
-    public override void AddRange(params object[] entities)
-    {
-        foreach (var entity in entities)
-        {
-            SetCreatedOnDateTime(entity);
-        }
-        base.AddRange(entities);
-    }
-
-    public override void AddRange(IEnumerable<object> entities)
-    {
-        foreach (var entity in entities)
-        {
-            SetCreatedOnDateTime(entity);
-        }
-        base.AddRange(entities);
-    }
-
-    public override Task AddRangeAsync(IEnumerable<object> entities, CancellationToken cancellationToken = new CancellationToken())
-    {
-        foreach (var entity in entities)
-        {
-            SetCreatedOnDateTime(entity);
-        }
-        return base.AddRangeAsync(entities, cancellationToken);
-    }
-
-    public override Task AddRangeAsync(params object[] entities)
-    {
-        foreach (var entity in entities)
-        {
-            SetCreatedOnDateTime(entity);
-        }
-        return base.AddRangeAsync(entities);
-    }
-
-    
-
-    public override EntityEntry Update(object entity)
-    {
-        SetUpdatedOnDateTime(entity);
-        return base.Update(entity);
-    }
-
-
-    public override void UpdateRange(params object[] entities)
-    {
-        foreach (var entity in entities)
-        {
-            SetUpdatedOnDateTime(entity);
-        }
-        base.UpdateRange(entities);
-    }
-
-    public override void UpdateRange(IEnumerable<object> entities)
-    {
-        foreach (var entity in entities)
-        {
-            SetUpdatedOnDateTime(entity);
-        }
-        base.UpdateRange(entities);
-    }
-
-    public override EntityEntry<TEntity> Update<TEntity>(TEntity entity)
-    {
-        SetUpdatedOnDateTime(entity);
-        return base.Update(entity);
     }
 
     public override int SaveChanges(bool acceptAllChangesOnSuccess)
     {
+        ChangeCreationAndUpdatedDateTime();
         return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    private void ChangeCreationAndUpdatedDateTime()
+    {
+        var changedEntriesCopy = this.ChangeTracker.Entries()
+            .Where(e => (e.State == EntityState.Added ||
+                         e.State == EntityState.Modified)
+                        && e.Entity is TimestampedEntity)
+            .ToList();
+        foreach (var entry in changedEntriesCopy)
+        {
+            var now = DateTime.Now;
+            var createdOnProperty = entry.Properties.FirstOrDefault(_ => _.Metadata.Name.ToLower().Equals("createdon"));
+            createdOnProperty.CurrentValue = now;
+
+            var updatedOnProperty = entry.Properties.FirstOrDefault(_ => _.Metadata.Name.ToLower().Equals("updatedon"));
+            updatedOnProperty.CurrentValue = now;
+        }
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+    {
+        ChangeCreationAndUpdatedDateTime();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
     }
 }
